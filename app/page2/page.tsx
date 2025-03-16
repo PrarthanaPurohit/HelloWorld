6914182138333789182d616e2c7b8c5ed2964473
Prarthana Purohit <prarthana.purohit03@gmail.com>
2025-03-16 17:25:20 +0530

    Fixed UI

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -8,17 +8,17 @@ import { useSearchParams } from "next/navigation";
 export default function Page2() {
   const searchParams = useSearchParams();
 
-  const first = searchParams.get("first");  
-  const last = searchParams.get("last");    
+  const first = searchParams.get("first");
+  const last = searchParams.get("last");
   const email = searchParams.get("email");
 
   return (
-    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <Typography>first: {first}</Typography>
-      <Typography>last: {last}</Typography>
-      <Typography>email: {email}</Typography>
-      
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper", p: 2 }}>
+      <Box sx={{ mt: 2 }}>
+        <Typography>first: {first}</Typography>
+        <Typography>last: {last}</Typography>
+        <Typography>email: {email}</Typography>
+      </Box>
     </Box>
   );
 }
-
